07958d4ff380559288bbd0cab2a8b39d051452c9
YudaiHayashi <yudai0913haya@gmail.com>
2019-05-25 15:37:19 +0900

    draw the data loaded

--- js/data.js
+++ js/data.js
@@ -1,14 +1,9 @@
 var json = {
             type: 'line',
             data: {
-                labels: [
-        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
-                ],
+                labels: [1,2,3,4,5],
                 datasets: [{
-                    //label: '支出',
-                    data: [
-        312,312,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-                    ],
+                    data: [0, 0, 0, 0 , 0],
                     borderWidth: 2,
                     strokeColor: 'rgba(0,0,255,1)',
                     backgroundColor: 'rgba(0,191,255,0.5)',
@@ -29,11 +24,11 @@ var json = {
                     }],
                     yAxes: [{
                         ticks: {
-                            beginAtZero:true
+                            beginAtZero:false
                         },
                         scaleLabel: {
                             display: true,
-                            labelString: 'Intensity',
+                            labelString: 'Intensity (mV)',
                             fontsize: 18
                         }
                     }]
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var upload = document.getElementById("draw-graph");
+var x = [];
+var y = [];
 
 upload.addEventListener("click", function() {
   var data = document.getElementById("upload-file");
@@ -6,5 +8,20 @@ upload.addEventListener("click", function() {
 
   var reader = new FileReader();
   reader.readAsText(data_content);
-  console.log(reader);
+  reader.onload = function () {
+    var value = reader.result;
+    var word_list = value.split('\n');
+    for (var i = 0; i < word_list.length - 1; i++){
+      var pair = word_list[i].split('\t');
+      x.push(parseFloat(pair[0])/1000);
+      y.push(parseFloat(pair[1])*1000);
+
+    json["data"]["labels"] = x;
+    json["data"]["datasets"][0]["data"] = y;
+  }
+
+  //console.log(x);
+  //console.log(y);
+  }
+
 });
